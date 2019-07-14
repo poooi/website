36d56fe598187241d14e9692deca1df65d29d8ec
Kagami <moe.pascal@gmail.com>
2019-07-15 03:36:00 +0800

    test: support canvas tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,6 +3,7 @@ import { setIconOptions } from 'office-ui-fabric-react'
 /* tslint:disable no-implicit-dependencies */
 import '@testing-library/jest-dom/extend-expect'
 import '@testing-library/react/cleanup-after-each'
+import 'jest-canvas-mock'
 import 'snapshot-diff' // for typings
 import 'snapshot-diff/extend-expect'
 
