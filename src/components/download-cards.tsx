69c0dc95d15a339097297d09c7cf0dc4dd6f9d0d
Kagami <moe.pascal@gmail.com>
2019-07-15 22:51:03 +0800

    refactor: remove cdd module for download card

--- src/components/download-cards.tsx
+++ src/components/download-cards.tsx
@@ -1,8 +1,6 @@
-import classNames from 'classnames'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-
-import styles from './download-cards.module.css'
+import styled from 'styled-components'
 
 const BASE_URI = 'https://npm.taobao.org/mirrors/poi'
 
@@ -30,6 +28,49 @@ function getDownloadLink(version: string, target: string) {
   }
 }
 
+const Container = styled.div`
+  text-align: center;
+  margin-bottom: 3em;
+`
+
+const Header = styled.div`
+  font-size: 22px;
+  margin-bottom: 2vh;
+
+  @media screen and (min-width: 768px) {
+    font-size: 26px;
+  }
+`
+
+const Button = styled.button<{ isBeta?: boolean }>`
+  background-color: rgba(0, 0, 0, 0);
+  border: #333 solid 1px;
+  border-radius: 1px;
+  color: #333;
+  cursor: pointer;
+  display: inline;
+  display: ${props => props.isBeta && 'none'};
+  font-size: 22px;
+  padding: 8px 12px;
+  margin: 4px 8px;
+  transition: all 300ms;
+  min-width: 12em;
+
+  :hover {
+    background-color: #333;
+    color: #fff;
+  }
+
+  @media screen and (min-width: 768px) {
+    display: ${props => props.isBeta && 'inline'};
+  }
+}
+`
+
+const Description = styled.div`
+  font-size: 14px;
+`
+
 interface IVersion {
   stable: string
   beta: string
@@ -44,24 +85,24 @@ interface IProps {
 export const DownloadCards = ({ target, version }: IProps) => {
   const { t } = useTranslation()
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
+    <Container>
+      <Header>
         {t('download-for')} {t(target)}
-      </div>
+      </Header>
       <a href={getDownloadLink(version.stable, target)}>
-        <button className={styles.button}>
+        <Button>
           <div>{version.stable}</div>
-          <div className={styles.description}>{t('stable-hint')}</div>
-        </button>
+          <Description>{t('stable-hint')}</Description>
+        </Button>
       </a>
       {version.betaAvailable && (
         <a href={getDownloadLink(version.beta, target)}>
-          <button className={classNames(styles.button, styles.beta)}>
+          <Button isBeta={true}>
             <div>{version.beta}</div>
-            <div className={styles.description}>{t('beta-hint')}</div>
-          </button>
+            <Description>{t('beta-hint')}</Description>
+          </Button>
         </a>
       )}
-    </div>
+    </Container>
   )
 }
