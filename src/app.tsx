3af54051f687679d291abab875221f2e51282306
Kagami <moe.pascal@gmail.com>
2019-10-24 20:23:50 +0800

    feat: detect dark theme

--- src/app.tsx
+++ src/app.tsx
@@ -42,12 +42,20 @@ const Wrapper = styled.div`
   flex-direction: column;
 `
 
-const choiceDark = localStorage.getItem('theme') === 'dark'
+const getDefaultIsDark = (): boolean => {
+  if (localStorage.getItem('theme')) {
+    return localStorage.getItem('theme') === 'dark'
+  }
+  return (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  )
+}
 
 export const App = () => {
   const [isDark, dispatch] = useReducer(
     (state: boolean, value: any) => !state,
-    choiceDark,
+    getDefaultIsDark(),
   )
 
   useEffect(() => {
