9dfcefa4bb72acdfa02b04b188588efaa4c7b313
かがみ <seki@kagami.me>
2019-11-03 08:08:56 +0800

    test: add jest styled components

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,6 +3,7 @@ import { setIconOptions } from 'office-ui-fabric-react'
 /* tslint:disable no-implicit-dependencies */
 import '@testing-library/jest-dom/extend-expect'
 import 'jest-canvas-mock'
+import 'jest-styled-components'
 import 'snapshot-diff' // for typings
 import 'snapshot-diff/extend-expect'
 
