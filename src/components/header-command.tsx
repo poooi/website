aa7f41c2c99f1f3fe6331d95bcf74a1d6756135a
Kagami <moe.pascal@gmail.com>
2020-01-05 03:31:48 +0800

    fix: disable eslint warnings from cra

--- src/components/header-command.tsx
+++ src/components/header-command.tsx
@@ -62,7 +62,7 @@ const HeaderCommand = () => {
         },
         text: value,
       })),
-    [i18n.changeLanguage],
+    [i18n.changeLanguage], // eslint-disable-line
   )
 
   useEffect(() => {
@@ -72,7 +72,7 @@ const HeaderCommand = () => {
       },
     })
     setFontfamily(getLocaleFontFamily(i18n.language))
-  }, [i18n.language])
+  }, [i18n.language]) // eslint-disable-line
 
   return (
     <CommandBar>
