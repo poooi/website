cb0a56ca2f43c56a4b84a99df65fe70d6d905914
Kagami <moe.pascal@gmail.com>
2020-06-21 05:12:36 +0800

    feat: remove bp icons

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+const { NormalModuleReplacementPlugin } = require('webpack')
 const withImages = require('next-images')
 const withCSS = require('@zeit/next-css')
 const { flow } = require('lodash')
 
 module.exports = flow([withImages, withCSS])({
-  outDir: 'build',
+  webpack: (config) => {
+    config.plugins.push(
+      new NormalModuleReplacementPlugin(
+        /.*\/generated\/iconSvgPaths.*/,
+        path.resolve(__dirname, 'src', 'empty-blueprint-icons.js'),
+      ),
+    )
+    return config
+  },
 })
