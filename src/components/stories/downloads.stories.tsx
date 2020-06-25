43e9c3a92fe3631a3232157be59a6c3f2d3c3177
Kagami <moe.pascal@gmail.com>
2020-06-25 17:43:52 +0800

    feat: add more stories

---
+++ src/components/stories/downloads.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Download from '../download'
+
+export default {
+  title: 'Download',
+  component: Download,
+}
+
+export const StableVersionOnly = () => (
+  <Download version={{ version: 'v10.2.4', betaVersion: 'v10.0.0-beta.1' }} />
+)
+
+export const BetaVersionAvailable = () => (
+  <Download version={{ version: 'v10.2.4', betaVersion: 'v11.0.0-beta.0' }} />
+)
