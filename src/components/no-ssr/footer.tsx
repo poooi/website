6bbdbaa4aab19409fc1164d1b3d0ef804e3881ba
かがみ <moe.pascal@gmail.com>
2020-08-31 01:12:05 +0800

    feat: update footer (#755)

--- src/components/no-ssr/footer.tsx
+++ src/components/no-ssr/footer.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components/macro'
+import { AnchorButton, ButtonGroup, Intent } from '@blueprintjs/core'
 
 import { faDiscord } from '@fortawesome/free-brands-svg-icons/faDiscord'
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
@@ -19,12 +20,8 @@ const Icon = ({ icon }: { icon: IconProp }) => (
 const Container = styled.div`
   text-align: center;
   min-height: 50px;
-  line-height: 50px;
-  margin-bottom: 1em;
 
   a {
-    margin: 0 1em;
-    transition: 0.3s;
     cursor: pointer;
     white-space: nowrap;
   }
@@ -44,31 +41,33 @@ export const Footer = () => {
   return (
     <Container>
       <Copyright>{`© ${new Date().getFullYear()} poi Contributors`}</Copyright>
-      {['zh-Hans', 'zh-Hant'].includes(lang) ? (
-        <>
-          <a href="http://weibo.com/letspoi">
-            <Icon icon={faWeibo} />
-            {t('weibo')}
-          </a>
-          <a href={t('telegram-group-link')}>
-            <Icon icon={faTelegram} />
-            {t('telegram')}
-          </a>
-        </>
-      ) : (
-        <a href={t('discord-channel-link')}>
-          <Icon icon={faDiscord} />
-          {t('Discord sub-channel')}
-        </a>
-      )}
-      <a href="https://github.com/poooi/poi">
-        <Icon icon={faGithub} />
-        {t('github')}
-      </a>
-      <a href="https://opencollective.com/poi">
-        <Icon icon={faHeart} />
-        {t('opencollective')}
-      </a>
+      <ButtonGroup minimal>
+        {['zh-Hans', 'zh-Hant'].includes(lang) ? (
+          <>
+            <AnchorButton href="http://weibo.com/letspoi">
+              <Icon icon={faWeibo} />
+              {t('weibo')}
+            </AnchorButton>
+            <AnchorButton href={t('telegram-group-link')}>
+              <Icon icon={faTelegram} />
+              {t('telegram')}
+            </AnchorButton>
+          </>
+        ) : (
+          <AnchorButton href={t('discord-channel-link')}>
+            <Icon icon={faDiscord} />
+            {t('Discord sub-channel')}
+          </AnchorButton>
+        )}
+        <AnchorButton href="https://github.com/poooi/poi">
+          <Icon icon={faGithub} />
+          {t('github')}
+        </AnchorButton>
+        <AnchorButton href="https://opencollective.com/poi">
+          <Icon icon={faHeart} />
+          {t('opencollective')}
+        </AnchorButton>
+      </ButtonGroup>
     </Container>
   )
 }
