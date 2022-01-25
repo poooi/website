ae276261636b3a4f5c83241d9be65ba09b2b24ff
Kagami <moe.pascal@gmail.com>
2022-01-25 23:26:11 +0800

    test: add e2e tests

--- packages/integration/specs/web/navigation.spec.ts
+++ packages/integration/specs/web/navigation.spec.ts
@@ -1,30 +1,32 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, Page } from '@playwright/test'
 
-test('test', async ({ page }) => {
-  await page.goto('http://127.0.0.1:8787/')
+const testExternalLink = async (page: Page, url: string) => {
+  const newPage = await page.context().waitForEvent('page')
+  await newPage.waitForLoadState()
+  await expect(newPage).toHaveURL(url)
+  await newPage.close()
+}
+
+test('Web navigation', async ({ page }) => {
+  await page.goto('http://127.0.0.1:3000/')
 
   await page.click('[data-testid="language-dropdown"]')
 
   await page.click('text=English')
 
   await page.click('button:has-text("Explore")')
-  await expect(page).toHaveURL('http://127.0.0.1:8787/explore')
+  await expect(page).toHaveURL('http://127.0.0.1:3000/explore')
 
   await page.click('button:has-text("Downloads")')
-  await expect(page).toHaveURL('http://127.0.0.1:8787/downloads')
+  await expect(page).toHaveURL('http://127.0.0.1:3000/downloads')
 
   await Promise.all([
-    (async () => {
-      const newPage = await page.context().waitForEvent('page')
-      await newPage.waitForLoadState()
-      await expect(newPage).toHaveURL('https://github.com/poooi/poi')
-      await newPage.close()
-    })(),
+    testExternalLink(page, 'https://github.com/poooi/poi'),
     page.click('a[role="button"]:has-text("GitHub")'),
   ])
 
   await Promise.all([
-    page.waitForNavigation({ url: 'https://opencollective.com/poi' }),
+    testExternalLink(page, 'https://opencollective.com/poi'),
     page.click('a[role="button"]:has-text("OpenCollective")'),
   ])
 })
