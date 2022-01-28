ae4d415e4029d844d66166322ead7602bd320874
Kagami <moe.pascal@gmail.com>
2022-01-29 02:10:55 +0800

    feat: allow handling other methods

---
+++ packages/integration/playwright.config.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { PlaywrightTestConfig } from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+  timeout: 120 * 1000,
+}
+
+export default config
